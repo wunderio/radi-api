300448942305e6b23b9cbb4b58ea78ae443aa25d
James Nesbitt <james.nesbitt@wunderkraut.com>
2016-10-27 09:25:55 +0300

    kraut split - kept only api components

---
+++ handler/handler.go
@@ -0,0 +1,20 @@
+package handler
+
+/**
+ * This file defines the core handle functionality, which
+ * actual handler implementations will implement.
+ *
+ */
+
+import (
+	"github.com/james-nesbitt/kraut-api/operation"
+)
+
+type Handler interface {
+	// Initialize and validate the Handler
+	Init() operation.Result
+	// Rturn a string identifier for the Handler (not functionally needed yet)
+	Id() string
+	// Return a list of Operations from the Handler
+	Operations() *operation.Operations
+}
