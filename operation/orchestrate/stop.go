073b722fd80969f200c69f0834db0f5a6688b67e
James Nesbitt <james.nesbitt@wunderkraut.com>
2016-12-14 16:04:22 +0200

    more orchestrate base ops, and an int base property

--- operation/orchestrate/stop.go
+++ operation/orchestrate/stop.go
@@ -1,1 +1,34 @@
 package orchestrate
+
+/**
+ * Orchestration START - like docker-compose stop
+ *
+ * Stop all app containers, volumes and networks.
+ */
+
+const (
+	OPERATION_ID_ORCHESTRATE_STOP = "orchestrate.stop"
+)
+
+// Base class for orchestration Up Operation
+type BaseOrchestrationStopOperation struct{}
+
+// Id the operation
+func (stop *BaseOrchestrationStopOperation) Id() string {
+	return OPERATION_ID_ORCHESTRATE_STOP
+}
+
+// Label the operation
+func (stop *BaseOrchestrationStopOperation) Label() string {
+	return "Stop"
+}
+
+// Description for the operation
+func (stop *BaseOrchestrationStopOperation) Description() string {
+	return "This operation will stop all containers, volumes and networks related to an application."
+}
+
+// Is this an internal API operation
+func (stop *BaseOrchestrationStopOperation) Internal() bool {
+	return false
+}
--- operation/property_base.go
+++ operation/property_base.go
@@ -142,6 +142,29 @@ func (property *BooleanProperty) Set(value interface{}) bool {
 	}
 }
 
+// A base Property that provides an Integer value
+type IntProperty struct {
+	value int
+}
+
+// Give an idea of what type of value the property consumes
+func (property *IntProperty) Type() string {
+	return "int"
+}
+
+func (property *IntProperty) Get() interface{} {
+	return interface{}(property.value)
+}
+func (property *IntProperty) Set(value interface{}) bool {
+	if converted, ok := value.(int); ok {
+		property.value = converted
+		return true
+	} else {
+		log.WithFields(log.Fields{"value": value}).Error("Could not assign Property value, because the passed parameter was the wrong type. Expected int")
+		return false
+	}
+}
+
 // A base Property that provides an IO.Writer
 type WriterProperty struct {
 	value io.Writer
